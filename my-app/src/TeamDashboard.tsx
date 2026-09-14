ea8f23f040b843486f6a710dfb7ff13f81e02ab8
Salomon Uwimana Masasu <180163847+masasu-salomon@users.noreply.github.com>
2026-09-14 10:59:12 +0200

    Return multiple dashboard elements from a React Fragment.
    Author: Salomon Uwimana Masasu

--- my-app/src/TeamDashboard.tsx
+++ my-app/src/TeamDashboard.tsx
@@ -3,13 +3,13 @@ import type { JSX } from 'react'
 
 function TeamDashboard(): JSX.Element {
   return (
-    <section>
+    <>
       <h1>Group 2 Team Dashboard</h1>
       <p>
         This application helps Group 2 organize members, track progress, and
         collaborate as we build our React and TypeScript project together.
       </p>
-    </section>
+    </>
   )
 }
 
